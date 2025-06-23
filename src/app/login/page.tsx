58144a76a8fedb427fb6485d45e5b2696cdce714
basakrdnz <basak.karadeniz0@gmail.com>
2025-06-23 12:11:13 +0300

    intro

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,23 @@ import { LoginForm } from "@/components/login-form";
 export default function LoginPage() {
   const router = useRouter();
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <LoginForm />
+    <main className="flex flex-col items-center my-4 justify-center min-h-screen">
+      <div 
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: "url('/bg_img_1.jpeg')",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          borderRadius: "50%",
+          width: "700px",
+          height: "700px",
+          margin: "auto"
+        }}
+      />
+      <div className="z-10">
+        <LoginForm />
+      </div>
     </main>
   );
 } 
